ef1a213909425ca1cfe98b048701659f6b9a501b
Wambo <Wambo@noreply.cppprojects.zapto.org>
2018-10-17 20:35:29 +0200

    Most events handled

--- shitcord/events/parser.py
+++ shitcord/events/parser.py
@@ -13,7 +13,24 @@ parsers = dict(
     message_delete=NullParser(),
     guild_member_update=NullParser(),
     guild_update=NullParser(),
-    message_update=NullParser()
+    message_update=NullParser(),
+    message_reaction_add=NullParser(),
+    message_reaction_remove=NullParser(),
+    message_reaction_remove_all=NullParser(),
+    channel_pins_update=NullParser(),
+    channel_create=NullParser(),
+    channel_update=NullParser(),
+    channel_delete=NullParser(),
+    guild_ban_add=NullParser(),
+    guild_ban_remove=NullParser(),
+    guild_member_remove=NullParser(),
+    guild_member_add=NullParser(),
+    guild_member_chunk=NullParser(),
+    guild_role_create=NullParser(),
+    guild_role_update=NullParser(),
+    guild_role_delete=NullParser(),
+    webhooks_update=NullParser()
+
 )
 
 
