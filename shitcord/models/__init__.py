1dd7cc74a2df8cc3ac4837d0fdd9b48689014cc4
G3bE <32980656+G3bE@users.noreply.github.com>
2018-10-17 16:28:58 +0200

    Some models

--- shitcord/models/__init__.py
+++ shitcord/models/__init__.py
@@ -1,4 +1,9 @@
 from .core import Model
 from .user import User
+from .Channel import Channel
+from .Role import Role
+from .Emoji import Emoji
+from .Guild import Guild
+from .Message import Message
 
-__all__ = ('Model', 'User',)
+__all__ = ('Model', 'User', 'Channel', 'Role', 'Emoji', 'Guild', 'Message',)
