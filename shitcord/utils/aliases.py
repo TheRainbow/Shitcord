a6adddc2baee8aa6d78f4c6370b8ee8c90dfe170
Wambo <Wambo@noreply.cppprojects.zapto.org>
2018-10-20 13:08:32 +0200

    Added some aliases

--- shitcord/utils/aliases.py
+++ shitcord/utils/aliases.py
@@ -1,3 +1,9 @@
 default_aliases = dict(
     message='message_create',
+    member_add='guild_member_add',
+    member_remove='guild_member_remove',
+    kick='guild_member_remove',
+    ban='guild_ban_add',
+    unban='guild_ban_remove',
+    typing='typing_start'
 )
