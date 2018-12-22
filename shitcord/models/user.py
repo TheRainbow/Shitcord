cfd063c909ec3ade4be948806afb7f696166c643
Vale <38182450+itsVale@users.noreply.github.com>
2018-12-22 21:52:46 +0100

    Further improvements on the User model + more methods and properties

--- shitcord/models/user.py
+++ shitcord/models/user.py
@@ -4,6 +4,8 @@ import enum
 
 from . import abc
 from .base import Model
+from .errors import NoFlags
+from ..utils import cdn
 
 __all__ = ['User']
 
@@ -32,25 +34,73 @@ class Flags(enum.IntEnum):
     EARLY_SUPPORTER      = 512
 
 
-class User(Model, abc.DiscordUser):
+class _BaseUser(Model, abc.DiscordUser):
     def __init__(self, data, http):
+        self._json = data
         super().__init__(data['id'], http=http)
 
         self.username = data['username']
-        self.discriminator = data['discriminator']
+        self.discriminator = int(data['discriminator'])
         self.avatar_hash = data['avatar']
+
+    def __str__(self):
+        return '{0.username}#{0.discriminator}'.format(self)
+
+    @property
+    def mention(self):
+        raise NotImplementedError
+
+    @property
+    def default_avatar(self):
+        return cdn.PlebAvatar(self.discriminator % 5)
+
+    @property
+    def avatar(self):
+        return self.avatar_url()
+
+    @property
+    def animated_avatar(self):
+        avatar_hash = self.avatar_hash
+        return avatar_hash and avatar_hash.startswith('a_')
+
+    @property
+    def default_avatar_url(self):
+        return cdn.format_url(cdn.Endpoints.DEFAULT_USER_AVATAR, dict(discriminator=self.default_avatar.value), image_format='png')
+
+    def avatar_url(self, fmt='webp', size=1024):
+        if not self.avatar_hash:
+            return self.get_default_avatar()
+
+        if self.animated_avatar:
+            # For the case of an animated avatar, always force the CDN URL to represent the gif format.
+            fmt = 'gif'
+            animated = True
+        else:
+            animated = False
+
+        return cdn.format_url(cdn.Endpoints.USER_AVATAR, dict(user=self.id, hash=self.avatar_hash), image_format=fmt, size=size, animated=animated)
+
+
+class User(_BaseUser, abc.DiscordUser):
+    def __init__(self, data, http):
+        super().__init__(data['id'], http)
+
         self.bot = data.get('bot', False)
         self.mfa_enabled = data.get('mfa_enabled', False)
         self.language = data.get('locale')
         self.verified = data.get('verified')
         self.email = data.get('email', '')
-        self.flags = data['flags']
+        self.flags = data.get('flags')
         self.premium_type = data.get('premium_type')
 
-    def __str__(self):
-        return '{0.username}#{0.discriminator}'.format(self)
+    @property
+    def mention(self):
+        return '<@{}>'.format(self.id)
 
     def _has_flag(self, flag):
+        if self.flags is None:
+            raise NoFlags('User object has no flags attached.')
+
         value = flag.value
         return (self.flags & value) == value
 
